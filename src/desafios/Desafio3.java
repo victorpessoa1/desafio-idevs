18dbdc4e2a6aa1624794198a4ba97a74b993e938
José Victor <josevitorppessoa@gmail.com>
2022-04-06 11:45:24 -0300

    desenvolvendo desafio 3
    ainda apanhando, mas finalmente evoluindo

--- src/desafios/Desafio3.java
+++ src/desafios/Desafio3.java
@@ -5,11 +5,13 @@ import javax.swing.JOptionPane;
 public class Desafio3 {
 
 	public static void main(String[] args) {
-		int cont;
-		int n = Integer.parseInt(JOptionPane.showInputDialog(null));
+		int cont, conts, maiorp = 0, soma = 0;
+		//int n = Integer.parseInt(JOptionPane.showInputDialog(null));
+		int n = 100;
 		System.out.println("Primos: ");
 		for (int i = 2; i <= n; i++) {
 			cont = 0;
+			
 			for (int j = 1; j <= i; j++) {
 				if((i % j) == 0) {
 					cont++;
@@ -19,8 +21,25 @@ public class Desafio3 {
 
 			if(cont == 2) {
 				System.out.print(" " + i );
+				soma = soma + i;
+				
+				for (int a = 2; a <= soma; a++) {
+					conts = 0;
+					
+					for (int b = 1; b <= a; b++) {
+						if((a % b) == 0) {
+							conts++;
+						}
+					}
+					
+					if (conts == 2) {
+						maiorp = soma;
+					}
+					
+				}
 			}
 		}
+		System.out.println("\n Maior primo abaixo de " + n + ": " + maiorp);
 	}
 
 }
