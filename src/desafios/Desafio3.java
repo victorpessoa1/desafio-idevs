f97dba8f9f176455e8240330612762a3bf13177e
José Victor <josevitorppessoa@gmail.com>
2022-04-06 12:03:51 -0300

    Desafio 3 concluído
    documentado porcamente porque eu não sei como digitar o que fiz ali, mas espero conseguir explicar direito antes do tempo acabar

--- src/desafios/Desafio3.java
+++ src/desafios/Desafio3.java
@@ -2,13 +2,11 @@ package desafios;
 
 import javax.swing.JOptionPane;
 
-public class Desafio3 {
+public class Desafio3 { // Sendo bem sincero, não sei muito bem explicar o que aconteceu aqui, só sei que deu certo.
 
 	public static void main(String[] args) {
-		int cont, conts, maiorp = 0, soma = 0;
-		//int n = Integer.parseInt(JOptionPane.showInputDialog(null));
-		int n = 100;
-		System.out.println("Primos: ");
+		int cont, conts = 0, maiorp = 0, soma = 0;
+		int n = Integer.parseInt(JOptionPane.showInputDialog(null));
 		for (int i = 2; i <= n; i++) {
 			cont = 0;
 			
@@ -20,8 +18,11 @@ public class Desafio3 {
 			
 
 			if(cont == 2) {
-				System.out.print(" " + i );
 				soma = soma + i;
+				if(soma >= n) {
+					soma = soma - i;
+					break;
+				}
 				
 				for (int a = 2; a <= soma; a++) {
 					conts = 0;
@@ -30,16 +31,16 @@ public class Desafio3 {
 						if((a % b) == 0) {
 							conts++;
 						}
-					}
-					
-					if (conts == 2) {
-						maiorp = soma;
-					}
+					}					
+										
+				}
+				if (conts == 2) {
+					maiorp = soma;
 					
 				}
 			}
 		}
-		System.out.println("\n Maior primo abaixo de " + n + ": " + maiorp);
+		System.out.println("número primo que é resultado da maior soma de números primos abaixo de " + n + ": " + maiorp);
 	}
 
 }
